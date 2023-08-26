5887d42bb4a222f995281c2f73cb6351858f6f39
ADollar4750 <alextdollar@gmail.com>
2023-08-26 14:23:41 -0600

    works

--- TextWriter/Program.cs
+++ TextWriter/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+string[] inputFile = File.ReadAllLines("input.txt");
+File.Create("lineLength.txt");
+
+for(int i = 0; i < inputFile.Length; i++)
+{
+    inputFile[i] = Convert.ToString(inputFile[i].Length);
+}
+
+File.WriteAllLines("lineLegths.txt", inputFile);
