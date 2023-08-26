d38e81b73e2a954d1e64a580a39ab677a2bfbeb8
ADollar4750 <alextdollar@gmail.com>
2023-08-26 12:56:21 -0600

    First commit?

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// string? test = Console.ReadLine();
+// Console.WriteLine($"{test}");
 
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
 
-string? test = Console.ReadLine();
-Console.WriteLine($"{test}");
+Console.WriteLine("Please enter an number: ");
+int usernumber = int.Parse(Console.ReadLine());
+//make sum a double because you can't add a int and a double xD
+double sum = 0;
+
+
+for (int i = 0; i < usernumber; i++)
+{
+    
+    var numbers = double.Parse(Console.ReadLine());
+    sum += numbers;
+}
+
+Console.WriteLine("The sum of the entered numbers is: " + sum);
